96b64c8ee954cbac42011edf5b990882c4c4f25f
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-08-18 08:01:30 -0700

    Removed pre-slashes from urls.py

--- personal_proj_back/user_app/urls.py
+++ personal_proj_back/user_app/urls.py
@@ -3,7 +3,7 @@ from .views import SignUp, Login, Logout, Info
 
 urlpatterns = [
     path('', Info.as_view()),
-    path('/signup/', SignUp.as_view(), name='signup'), # name='signup'
-    path('/login/', Login.as_view(), name='login'), # name='login'
-    path('/logout/', Logout.as_view(), name='logout') # name='logout'
+    path('signup/', SignUp.as_view(), name='signup'), # name='signup'
+    path('login/', Login.as_view(), name='login'), # name='login'
+    path('logout/', Logout.as_view(), name='logout') # name='logout'
 ]
