4d7db06602a605c481a0b3578c7f0d70160302bf
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-08-20 09:37:22 -0700

    adding in more specifics for models.py

--- personal_proj_back/user_app/models.py
+++ personal_proj_back/user_app/models.py
@@ -1,14 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-
+from .validators import validate_user_email
+from django.core import validators as val
 # Create your models here.
 # refactor the username to equal email - thought I did that already?
 class User(AbstractUser):
 
-    email = models.EmailField(unique=True, max_length=2000)
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    email = models.EmailField(
+        unique=True, max_length=200,
+        default = None,
+        null = False,
+        blank = False,
+        validators = [
+            validate_user_email
+        ]
+    )
+
+    password = models.EmailField(
+        default=None,
+        blank = False,
+        null = False,
+        unique = True,
+        validators = [
+            val.MinValueValidator(2, "Password must be greater than or equal to 2 characters."),
+            val.MaxValueValidator(200, "Password must be less than or equal to 200 characters.")
+        ]
+    )
+    # USERNAME_FIELD = 'email'
+    # REQUIRED_FIELDS = []
 
 # I'm receiving the error, "value type too long for character(150)"
 # Even after adding in the max_lenght, I'm still getting the above error
