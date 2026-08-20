07d232b3eade46a0efddfeaea7c17bdeabcf9294
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-08-20 12:09:03 -0700

    changed up the catdeetspage

--- personal_proj_front/src/pages/HttpCatDeetsPage.jsx
+++ personal_proj_front/src/pages/HttpCatDeetsPage.jsx
@@ -1,22 +1,53 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { useParams, useOutletContext } from 'react-router-dom';
+import CatCard from '../components/Search/CatCard';
 
 export default function HttpCatDeetsPage() {
 
-    const [httpCat] = useState(null);
+    const [httpCat, setHttpCat] = useState(null);
     // const [errorMessage, setErrorMessage] = useState("");
     // const {id} = useParams();
-    const {list, addHttpCat, removeHttpCat, hasHttpCat} = useOutletContext();
-
+    const {team, addHttpCat, removeHttpCat, hasHttpCat} = useOutletContext();
+    const [errorMessage, setErrorMessage] = useState("");
     const isCaught = httpCat ? hasHttpCat(HttpCatDeetsPage.id): false;
-    const canCatch = httpCat ? !isCaught && list.length < 10 : false;
+    const canCatch = httpCat ? !isCaught && team.length < 10 : false;
    
+    useEffect(() => {
+        const lookupId = id?.charAt(0) + id?.slice(1) || "";
+        
+    
 
+
+    const fetchHttpCat = async () => {
+        try{
+            const response = await axios.get(
+                `https://http.cat/${lookupId}`
+            )
+            setHttpCat(response.data);
+            setErrorMessage("");
+        }catch (error) {
+            setHttpCat(null);
+            setErrorMessage(`No such HTTP Cat with id of '${lookupId}' exists.`);
+        }
+    };
+    fetchHttpCat();
+}, [id]);
+
+
+
+    if (errorMessage) {
+        return <MissingPage message={errorMessage}/>
+    }
+    if (!httpCat) {
+        return <MissingPage message={errorMessage}/>
+    }
    
     return (
         <div className="cat-ard">
             <h2>HTTP Status {httpCat}</h2>
             <img src={`https://http.cat/${httpCat}`}
-             alt={`HTTP ${httpCat}`}
+             alt={httpCat}
              />
 
             <button 
