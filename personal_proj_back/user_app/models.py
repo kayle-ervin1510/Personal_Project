120e08316a4e3884da9a32aa0925f3471f2e565a
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-08-20 09:31:58 -0700

    adding in another comment. also got my dns domain up and running from duck dns

--- personal_proj_back/user_app/models.py
+++ personal_proj_back/user_app/models.py
@@ -6,8 +6,9 @@ from django.contrib.auth.models import AbstractUser
 # refactor the username to equal email - thought I did that already?
 class User(AbstractUser):
 
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, max_length=2000)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
 # I'm receiving the error, "value type too long for character(150)"
+# Even after adding in the max_lenght, I'm still getting the above error
