d0f3ebbd24a706a55c54ed6cdda85d8989c00d13
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-08-20 06:23:34 -0700

    commenting out the resolve dedupe

--- personal_proj_front/vite.config.js
+++ personal_proj_front/vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    dedupe: ['react', 'react-dom'],
+  // resolve: {
+  //   dedupe: ['react', 'react-dom'],
     
-  },
+  // },
   plugins: [react()],
   server: {
     proxy: {
@@ -14,3 +14,4 @@ export default defineConfig({
     }
   }
 })
+// I have the dedupe to get rid of any duplicates of react and react-dom
