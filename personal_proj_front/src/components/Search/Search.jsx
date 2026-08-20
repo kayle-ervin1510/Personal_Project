58a4ccba35f64841700e8193d73b6237b68c3448
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-08-20 07:55:08 -0700

    had to define team in search

--- personal_proj_front/src/components/Search/Search.jsx
+++ personal_proj_front/src/components/Search/Search.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 
 export default function Search() {
     const [httpCatName, setHttpCatName] = useState("");
-    const [team] = useState();
+    const [team] = useState([]);
     const navigate = useNavigate();
 
 
