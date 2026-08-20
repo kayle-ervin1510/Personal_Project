63f8ba6b0d2369945e3851726d9c43444e3c3ca2
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-08-20 09:40:30 -0700

    fix didn't work

--- personal_proj_back/user_app/models.py
+++ personal_proj_back/user_app/models.py
@@ -6,28 +6,29 @@ from django.core import validators as val
 # refactor the username to equal email - thought I did that already?
 class User(AbstractUser):
 
-    email = models.EmailField(
-        unique=True, max_length=200,
-        default = None,
-        null = False,
-        blank = False,
-        validators = [
-            validate_user_email
-        ]
-    )
+    # email = models.EmailField(
+    #     unique=True, max_length=200,
+    #     default = None,
+    #     null = False,
+    #     blank = False,
+    #     validators = [
+    #         validate_user_email
+    #     ]
+    # )
 
-    password = models.EmailField(
-        default=None,
-        blank = False,
-        null = False,
-        unique = True,
-        validators = [
-            val.MinValueValidator(2, "Password must be greater than or equal to 2 characters."),
-            val.MaxValueValidator(200, "Password must be less than or equal to 200 characters.")
-        ]
-    )
-    # USERNAME_FIELD = 'email'
-    # REQUIRED_FIELDS = []
+    # password = models.EmailField(
+    #     default=None,
+    #     blank = False,
+    #     null = False,
+    #     unique = True,
+    #     validators = [
+    #         val.MinValueValidator(2, "Password must be greater than or equal to 2 characters."),
+    #         val.MaxValueValidator(200, "Password must be less than or equal to 200 characters.")
+    #     ]
+    # )
+    email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
 # I'm receiving the error, "value type too long for character(150)"
 # Even after adding in the max_lenght, I'm still getting the above error
