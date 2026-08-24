57e702408954e658b29472793ea4157cb1c2be1b
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-08-24 09:55:37 -0700

    editing things a little

--- personal_proj_front/src/components/Search/Form.jsx
+++ personal_proj_front/src/components/Search/Form.jsx
@@ -4,12 +4,13 @@ export default function Form({ handleSubmit, setHttpCatId, httpCatId}) {
         onSubmit={(event) => handleSubmit(event)} 
         className="mb-6 p-6 rounded-x1"
         >
+            <div>
             <input
                 id="http-number"
                 type="text"
                 name="name"
-                placeholder="Try '308'"
-                value={httpCatId ?? ""}
+                placeholder="Try 308"
+                value={httpCatId}
                 onChange={(e) => setHttpCatId(e.target.value)}
                 className="flex-1 rounded-md border border-slate-300 px-3 py-1 outline-none"
             />
@@ -17,6 +18,8 @@ export default function Form({ handleSubmit, setHttpCatId, httpCatId}) {
             type="submit"
             className="font-semibold text-black"
             >Search!</button>
+            </div>
+            <p className="mt-2 text-sm text-black">Search for HTTP Cats!</p>
         </form>
     );
 }
