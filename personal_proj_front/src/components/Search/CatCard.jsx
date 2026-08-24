3562434aaeae4c52def6e9c47d8b958fd070d49a
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-08-24 08:15:59 -0700

    comments and experimenting

--- personal_proj_front/src/components/Search/CatCard.jsx
+++ personal_proj_front/src/components/Search/CatCard.jsx
@@ -5,7 +5,7 @@ import Stack from 'react-bootstrap/Stack';
 
 export default function CatCard ({httpCat}){
     const [capture, setCapture] = useState(false);
-    // const imageSource = `https://http.cat/${httpCat}`
+    const imageSource = `https://http.cat/${httpCat}`
     
     return (
         <Card style={ {width:"18rem"} } id={ `cat-${httpCat}-card` }>
@@ -34,12 +34,6 @@ export default function CatCard ({httpCat}){
     )
 }
 
-// for HTTP Cats, the url usage is: https://http.cat/[status_code]
-// httpCat is a stand-in for the status code
-// so .id won't be of use, because it has no call for "id"
-// I need to call and list the image as an image
-// not using id, or name
-// the stand-in for the http status code is undefined, or null
-// if I put null in the useState for httpCat
-// I need to figure out how to make it equal to the status codes
-// the actual status codes
+// httpCat is listed as undefined in CatCard
+// as well as in all of the Search function
+// but in HttpCatDeetsPage it's no longer undefined
