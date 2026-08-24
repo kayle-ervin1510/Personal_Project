14f9e49e5d2f809da4580d513cc40e2618b96d07
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-08-24 11:53:08 -0700

    adding something

--- personal_proj_front/src/pages/AboutPage.jsx
+++ personal_proj_front/src/pages/AboutPage.jsx
@@ -12,7 +12,7 @@ export default function AboutPage() {
                     add them to a list, and write notes for easy reference.
                     To see the api used, please click the link below!
                 </p>
-                <a href="https://http.cat/">https://http.cat/</a>
+                <a href="https://http.cat/">http.cat</a>
               
         </>
     )
