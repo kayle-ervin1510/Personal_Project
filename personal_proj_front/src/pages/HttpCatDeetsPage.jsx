0c4b467ab646ed65ae59ecec7555a5203dda3ede
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-08-24 12:31:32 -0700

    took out an axios import that wasn't needed

--- personal_proj_front/src/pages/HttpCatDeetsPage.jsx
+++ personal_proj_front/src/pages/HttpCatDeetsPage.jsx
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useParams, useOutletContext } from 'react-router-dom';
 import CatCard from '../components/Search/CatCard';
