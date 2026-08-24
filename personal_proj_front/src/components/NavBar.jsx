fe8ff54345e7c2d55016476a4f4084a93504d4f3
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-08-24 12:00:35 -0700

    just trying something

--- personal_proj_front/src/components/NavBar.jsx
+++ personal_proj_front/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Search from './Search/Search';
-import BackButton from './BackButton';
+
 
 export default function NavBar() {
     return (
