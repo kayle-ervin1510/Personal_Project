16fcbbb03d4020aa9c577a73d621f16b6234336e
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-08-24 09:39:27 -0700

    this provides me at least an error, but doesn't let me see the 404 thing

--- personal_proj_front/src/components/Search/Search.jsx
+++ personal_proj_front/src/components/Search/Search.jsx
@@ -1,23 +1,45 @@
 import Container from './Container';
 import Form from './Form';
 import { useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams, useOutletContext} from 'react-router-dom';
 
 export default function Search() {
-    const [httpCatId, setHttpCatId] = useState(null);
+    const [httpCatId, setHttpCatId] = useState("");
+    // const {team, setTeam} = useOutletContext();
     const [team] = useState([]);
     const navigate = useNavigate();
     const {id} = useParams();
-    
+ // from here   
+    const addCatData = (data)=>{
+        setTeam([...team, data])
+    }
+
+    const rmData = (id)=>{
+        setTeam(team.filter((httpCat)=>httpCat.id!==id))
+    }
+    const getCatData = async ()=> {
+        const requestURL = `https://http.cat/${httpCatId}`
+        try{
+            let response = requestURL
+            console.log(response)
+            addCatData(response.data)
+        }catch(err){
+            console.log(err)
+            alert("Cat does not exist")
+        }finally{
+            console.log("That be all")
+        }
+    }
 
     const handleSubmit = (event) =>{
         event.preventDefault();
-        if(!httpCatId) return
+        getCatData()
+        // if(!httpCatId) return
 
-        navigate(`/cat/${httpCatId}`);
+        // navigate(`/cat/${httpCatId}`);
         setHttpCatId("");
     }
-
+// to here
     return (
         <>
             <Form
