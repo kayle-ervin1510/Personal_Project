f1641c228647e22bb097defba35311e56f7ca2c8
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-08-26 11:47:59 -0700

    finishing up the refresh token stuff

--- personal_proj_back/user_app/urls.py
+++ personal_proj_back/user_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import CreateUser, Login, Logout, Info #, RefreshView
+from .views import CreateUser, Login, Logout, Info, RefreshView
 
 urlpatterns = [
     path('', Info.as_view()),
     path('create/', CreateUser.as_view()), # name='create'
     path('login/', Login.as_view()), # name='login'
     path('logout/', Logout.as_view()), # name='logout'
-#   path('refresh/', RefreshView.as_view()) 
+  path('refresh/', RefreshView.as_view()) 
 ]
