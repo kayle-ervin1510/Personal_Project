8fd5d5e50b75d47ee19cec0903c2789f2f9ae38f
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-09-02 12:23:17 -0700

    installed cypress and began creation of a new cypress test

---
+++ personal_proj_front/cypress_tests/cypress_test_03_sign_in_add_cat_to_list.jsx
@@ -0,0 +1,8 @@
+describe("Test 3", ()=>{
+    it("Should be able to sign up, sign in, and add a cat to a list and view cat in MyList", ()=>{
+        cy.visit('/sign-up');
+        
+    })    
+
+
+})
