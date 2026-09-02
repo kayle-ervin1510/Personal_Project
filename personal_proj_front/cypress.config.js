761d4dd4ef0aebdfab008b27f01557354cb6671d
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-09-02 14:05:15 -0700

    updating config file

--- personal_proj_front/cypress.config.js
+++ personal_proj_front/cypress.config.js
@@ -1,16 +1,28 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+  allowCypressEnv: false,
 
-  component: {
-    devServer: {
-      framework: "react",
-      bundler: "vite",
-    },
+  e2e: {
+    baseURL: "http://localhost:5173/",
+    supportFile: false
   },
+  viewportWidth:1024,
+  viewportWidth:768,
+  video:false
+
+
+  // old code:
+  // e2e: {
+  //   setupNodeEvents(on, config) {
+  //     // implement node event listeners here
+  //   },
+  // },
+
+  // component: {
+  //   devServer: {
+  //     framework: "react",
+  //     bundler: "vite",
+  //   },
+  // },
 });
