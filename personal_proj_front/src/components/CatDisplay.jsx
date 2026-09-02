e7a9afedb7a6eb2cd345b5fe75fe095832be416a
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-09-02 08:51:33 -0700

    took out some uneeded commented out code

--- personal_proj_front/src/components/CatDisplay.jsx
+++ personal_proj_front/src/components/CatDisplay.jsx
@@ -1,27 +1,11 @@
 import Stack from 'react-bootstrap/Stack';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import { useState } from 'react';
 import { updateCat as updateCatRequest, deleteCat as deleteCatRequest } from '../user_utilities';
 
 const CatDisplay = ({cat, rmHttpCat, addCat, setAddCat})=> {
 
 
-    // const [addCat, setAddCat] = useState(cat.title)
- 
-    
-    // const addCatHandle = async ()=> {
-    //     const addedCat = await updateCatRequest(
-    //         {
-    //             id:cat.id,
-    //             title: addCat
-    //         }
-    //     )
-    //     if (addedCat){
-    //         updateCat(addedCat)
-    //         setAdd(true)            
-    //     }
-    // }
 
     const deleteCatHandle = async () => {
         const wasDeleted = await deleteCatRequest(cat.id)
@@ -40,8 +24,7 @@ const CatDisplay = ({cat, rmHttpCat, addCat, setAddCat})=> {
                 style={{width:"18rem"}}
                 className="cat-auto"
                 placeholder={cat.title}
-                // value={addCat}
-                // onChange={(e)=>setAddCat(e.target.value)}
+                
                 />
                 
                 
@@ -49,7 +32,6 @@ const CatDisplay = ({cat, rmHttpCat, addCat, setAddCat})=> {
                     </>
                 <>
                 <img style={ {width:"18rem"} }src={`https://http.cat/${cat.title}`} alt={`HTTP Cat ${cat.title}`}/>
-                {/* <Button variant="outline-primary" onClick={addCatHandle}>Add</Button> */}
                     <div className="vr"/>
                 <div >{cat.title}</div>
                 <div className="vr"/>
