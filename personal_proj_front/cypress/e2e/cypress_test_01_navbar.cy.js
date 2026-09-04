bad9b052d4bd6ca7a737606a38aa3d2714eb27b8
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-09-04 14:30:39 -0700

    fixed an error

--- personal_proj_front/cypress/e2e/cypress_test_01_navbar.cy.js
+++ personal_proj_front/cypress/e2e/cypress_test_01_navbar.cy.js
@@ -3,9 +3,9 @@ describe("Test 1", () => {
     it("will test the structure of the navbar", () => {
         cy.visit("http://localhost:5173");
         cy.get("nav").should("exist");
-        cy.get("nav h1").should("have.text", "Navigation Bar")
+        cy.get("nav h3").should("have.text", "Navigation Bar")
 
-        cy.get("nav input").should("have.atrr", "placeholder", "Try 308");
+        cy.get("nav input").should("have.attr", "placeholder", "Try 308");
 
         cy.get("nav button").should("have.text", "Search!");
     });
