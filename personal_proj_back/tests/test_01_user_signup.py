3567f38334077f9fe1bb4f965ef1ad7e0a2726b6
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-09-06 11:04:43 -0700

    trying something else

--- personal_proj_back/tests/test_01_user_signup.py
+++ personal_proj_back/tests/test_01_user_signup.py
@@ -5,7 +5,7 @@ class Test_user_sign_up(TestCase):
     def test_001_user_sign_up(self):
         user = Client()
         response = user.post(
-            reverse("create_user"),
+            reverse("signup/"),
             data = {"email":"je@je.com", "password":"je"},
             content_type = "application/json",
         )
