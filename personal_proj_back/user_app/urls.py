0a50bfa814d57aef0b5d6f9dd82912e5d8f410ba
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-09-07 09:03:26 -0700

    added names to my urls

--- personal_proj_back/user_app/urls.py
+++ personal_proj_back/user_app/urls.py
@@ -3,8 +3,8 @@ from .views import CreateUser, Login, Logout, Info, RefreshView
 
 urlpatterns = [
     path('', Info.as_view()),
-    path('create/', CreateUser.as_view()), # name='create'
-    path('login/', Login.as_view()), # name='login'
-    path('logout/', Logout.as_view()), # name='logout'
-  path('refresh/', RefreshView.as_view()) # name='refresh'
+    path('create/', CreateUser.as_view(), name='create'),
+    path('login/', Login.as_view(), name='login'),
+    path('logout/', Logout.as_view(), name='logout'),
+    path('refresh/', RefreshView.as_view(), name='refresh')
 ]
